7134ce141a00434bfe4ab1e6cbbda98280cc6f2d
jonahywu <152441918+jonahywu@users.noreply.github.com>
2023-11-30 20:53:00 -0500

    Add files via upload

---
+++ Lab03 Windows/info.py
@@ -0,0 +1,84 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/faceshot.jpg"
+about_me = "Hi! My name is Jonah Wu, and I am an Industrial Engineering major at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/jonah-y-wu"
+my_github_url = "https://github.com/jonahywu"
+my_email_address = "jonah.wu3533@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "ACCT 2101", "MATH 1554", "MATH 2551",], 
+    "names":["Intro to CS", "Accounting I", "Linear Algebra", "Multivariable Calculus"], 
+    "semester_taken":["2nd", "2nd", "1st", "1st"],
+    "skills":["Python, HTML, CSS, Java, OOP", "Financial Accounting, Transactions, Financial Statements", "Linear Systems, Matrices, Eigenvalues, Orthogonality", "Vector & Space Geometry, Double & Triple Integrals, Surfaces"],
+    }
+experience_data = {
+    "Back of House staff member at Chick-fil-A": (["- Supported FOH workers and BOH kitchen team with food preparation",
+                                                                          "- Assembled food & drink orders safely, quickly, and efficiently for up to 400 customers per shift", "- Mastered cleaning and closing procedures on multiple stations, as well as breakfast and lunch rush procedures"],"images/cfasandwich.jpg"),
+    "Private Academic Tutor": (["- Mentored students in higher level STEM courses, implementing effective SAT and ACT preparation strategies",
+                                                           "- Instructed study methods that empowered students to excel in coursework and tests"],"images/tutoring.jpg"),
+    "Ultimate Frisbee Captain": (["- Organized weekly practices and drills for a team of 25 players",
+                                                           "- Directed recruitment efforts that increased club membership by over 400%", "- Organized matches between 5 different schools and attended 2 competitive ultimate tournaments"],"images/frisbee.jpg"),
+}
+
+projects_data = {
+    "CS 1301 Web Development Labs": "Learned the basics of web development and created multiple online interactive forms and customized websites",
+    "GT 1000 Research Project": "Performed research on the impact of Industrial Engineering in the fields of healthcare, public health, and health services",
+    "CS 1301 App Development Project": "Created a model fitness tracker app using streamlit and Python"
+}
+
+programming_data = {
+    "Python": 80,
+    "HTML": 50,
+    "CSS": 30,
+    "Java": 10
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "HTML": "☕",
+    "CSS": "🔍",
+    "Java": "🔥"
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Chinese":"🇨🇳"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Native Fluency",
+    "Chinese": "Native Fluency",
+    "French": "Partial Fluency",
+}
+leadership_data = {
+    "Principal Trumpet Section Leader": (["- Selected after rigorous auditions for two Atlanta honor ensembles to perform with other talented musicians and renowned conductors on a national level",
+                                                           "- Performed at Carnegie Hall in July 2023 as part of an invitational program to show youth musical talent in the Atlanta area", "- Managed absences of section members during rehearsal and distribution of music and instrument supplies"], "images/ayws.jpg"),
+    "Academic Chair of Delta Sigma Phi Fraternity": (["- Kept pledges (and underperforming brothers) accountable for their grades and academic performance",
+                                                           "- Organized fraternity documents within accessible shared online folders", "- Promoted tutoring services and incentives to boost fraternity academic performance"], "images/academics.jpg")
+}
+activity_data={
+    "Church Worship Team Member": ["- Played trumpet and piano for holiday services as well as multiple weekly Chinese worship services", 
+            "- Rehearsed with an orchestral group + choir around twice per month"],
+    "Academic Tutoring Club Co-president": ["- Guided 100+ students in schoolwork, emphasizing standardized test preparation and study/test-taking strategies",
+            "- Led 4 sessions per semester with a focus on standardized test preparation (AP, IB, and SAT/ACT) and study/test-taking strategies", "- Recruited a team of 8 qualified student tutors to expand coverage into additional courses"]
+}
